894acf7edd2055b1c981e7ba5222124575eed887
jonathanschroeter <jonny.schroeter@gmail.com>
2021-04-10 20:01:59 -0500

    this is a test

---
+++ mod-v6.cc
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+	cout << "Hello World" << endl;
+
+	const char *command = "open() test.txt";
+	system("xdg-open ./test.txt");
+}
