246a034e1855c80f3117d96eea7ec8dd41cc64bb
CouchDroids <firepower1@sbcglobal.net>
2021-04-10 20:17:32 -0500

    ok heres a message

--- mod-v6.cc
+++ mod-v6.cc
@@ -4,8 +4,8 @@ using namespace std;
 
 int main(){
 	cout << "Hello World" << endl;
-
+	cout << "lolo pls boi" << endl;
 	const char *command = "open() test.txt";
 	system("xdg-open ./test.txt");
-	cout << "hello my name is charles" << endl;
+	cout << "hello my name is charles PLEASE I DID OK GOD" << endl;
 }
